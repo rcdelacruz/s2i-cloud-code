b5e28e6033f0767d53711b6ac933b68de4d92f85
root <rcdelacruz@gmail.com>
2020-08-20 20:15:39 +0800

    Fixed issues

--- root/opt/app-root/etc/post-script.sh
+++ root/opt/app-root/etc/post-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i '1s/^/require(".\/send-mail");\n/' /opt/app-root/src/cloud/main.js
-cp send-mail.js /opt/app-root/src/cloud
-cp adapter.js /opt/app-root/src/cloud
+cp /opt/app-root/etc/send-mail.js /opt/app-root/src/cloud
+cp /opt/app-root/etc/adapter.js /opt/app-root/src/cloud
