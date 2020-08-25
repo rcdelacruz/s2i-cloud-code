11c37f81072eb2c42e90c4fe64302145b73ad34d
root <rcdelacruz@gmail.com>
2020-08-25 15:21:28 +0800

    Updated post script

--- root/opt/app-root/etc/parse/src/cloud/send-mail.js
+++ root/opt/app-root/etc/parse/src/cloud/send-mail.js
@@ -1,18 +1,17 @@
 /*
 * this should be part of s2i
 */
-const path = require("path");
+//const path = require("path");
 const mailAdapter = require('./adapter');
 
 Parse.Cloud.define("sendMail", (req, res) => {
+  //console.log(req.params.to);
   mailAdapter.adapter.sendMail({
       from: process.env.EMAIL_FROM_ADDRESS,
-      to: 'rdelacruz@stratpoint.com', // "foo <foo@example.com>"
-      subject: 'this is a subject', // email subject
-      template: './public/files/templates/verifyUser.html',
-      templateData: {
-        /*template data*/
-      } // now you can use {{paramater}} in your template
+      to: req.params.to,//'rdelacruz@stratpoint.com', // "foo <foo@example.com>"
+      subject: req.params.subject, //'this is a subject', // email subject
+      template: req.params.templatePath, //'./public/files/templates/verifyUser.html',
+      templateData: req.params.templateData
     })
     .then(info => console.log("success"))
     .catch(err => console.log(err));
